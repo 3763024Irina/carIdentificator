8b1b3ae9659d71300e2d05d15e1ff6c0e5b98b81
IriSakovich <sakovich@gmx.es>
2025-03-31 18:34:04 +0200

    Initial Commit

---
+++ carIdentificator/carIdentificatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  carIdentificatorApp.swift
+//  carIdentificator
+//
+//  Created by Irina on 31/3/25.
+//
+
+import SwiftUI
+
+@main
+struct carIdentificatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
